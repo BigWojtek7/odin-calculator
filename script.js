422bd54872adb12dc88070f45af547099cd5e687
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-02-18 15:21:22 +0100

    Change buttons content to variables

--- script.js
+++ script.js
@@ -25,20 +25,36 @@ const operate = function(num1, num2, operator) {
     subtract(num1, num2);
   } else if (operator === "*"){
     multiply(num1, num2);
-  } else if (operator === "/"){
+  } else if (operator === "÷"){
     divide(num1, num2);
   }
 }
-let buttonContent = "";
-const buttons = document.querySelectorAll("button");
-const display = document.querySelector(".display")
 
+const events = function() {
+  let buttonContent = "";
+  const buttons = document.querySelectorAll("button");
+  const display = document.querySelector(".display")
 
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    buttonContent += (e.target.textContent);
-    console.log(buttonContent);
-
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (e.target.textContent === "=") userInput(buttonContent);
+      buttonContent += (e.target.textContent);
+  
+      display.textContent = buttonContent;
+    });
   });
-});
+}
+
+const userInput = function(buttonClick) {
+  let num1 = Number(buttonClick.match(/\d*/).join(""));
+  let num2 = Number(buttonClick.match(/(?<=[+|\-|÷|x]).*/).join(""));
+  let operator = buttonClick.match(/[+|\-|÷|x]/).join("");
+  console.log(typeof num1, typeof num2, typeof operator);
+  operate(num1, num2, operator);
+}
+
+
+
+
+
+events()
